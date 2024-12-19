f0c806720ac5c0ae333a1bdb0a421eda6f5b6fb2
matheins <mail@mheins.de>
2024-12-19 20:32:07 +0100

    wip

--- src/components/user-button.tsx
+++ src/components/user-button.tsx
@@ -2,14 +2,27 @@
 
 import { UserButton as ClerkUserButton, useAuth } from "@clerk/nextjs";
 
+const DotIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 512 512"
+			fill="currentColor"
+		>
+			<title>Dot Icon</title>
+			<path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+		</svg>
+	);
+};
+
 export function UserButton() {
 	const { signOut } = useAuth();
 	return (
 		<ClerkUserButton>
 			<ClerkUserButton.MenuItems>
 				<ClerkUserButton.Action
-					label="Open chat"
-					labelIcon={undefined}
+					label="Logout"
+					labelIcon={<DotIcon />}
 					onClick={() => signOut()}
 				/>
 			</ClerkUserButton.MenuItems>
