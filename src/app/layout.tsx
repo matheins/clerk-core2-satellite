468020497b2849c3675a475208915424737c2db8
matheins <mail@mheins.de>
2024-12-19 20:06:23 +0100

    fix: import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
-import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
-import "../globals.css";
-import { headers } from "next/headers";
-import { getApexDomainFromHost } from "@/utils";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import "./globals.css";
 import AuthProvider from "@/components/auth-provider";
 
 export default async function RootLayout({
@@ -9,30 +7,7 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	// const headersList = await headers();
-	// const host =
-	// 	headersList.get("x-forwarded-host") ??
-	// 	(process.env.NEXT_PUBLIC_ROOT_DOMAIN as string);
-	// const isSatellite = !host.includes(
-	// 	process.env.NEXT_PUBLIC_ROOT_DOMAIN as string,
-	// );
-
-	// console.log("root layout", {
-	// 	host,
-	// 	isSatellite,
-	// 	rootDomain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-	// });
-
-	// const domain = getApexDomainFromHost(host);
-
 	return (
-		// <ClerkProvider
-		// 	// does not work: Error: Functions cannot be passed directly to Client Components unless you explicitly expose it by marking it with "use server"
-		// 	// domain={(url) => url.host}
-		// 	domain={domain}
-		// 	isSatellite={isSatellite}
-		// 	afterSignOutUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL as string}
-		// >
 		<AuthProvider>
 			<html lang="en">
 				<body>
@@ -43,6 +18,5 @@ export default async function RootLayout({
 				</body>
 			</html>
 		</AuthProvider>
-		// </ClerkProvider>
 	);
 }
